28c6b4a2d6295fb82cae6d0702a8e37624c6c75a
lynx-r1 <lynx-r1@yandex.ru>
2010-09-25 21:20:27 +0400

    Added fade effect for main page items

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,34 +1,71 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+var fadeToUp = 1;
+var fadeToDown = 0.7;
+var fadeDuration = 500;
+
+function fadeDown(obj) {
+    $(obj).stop().fadeTo(fadeDuration, fadeToDown);
+}
+
+function fadeUp(obj) {
+    $(obj).stop().fadeTo(fadeDuration, fadeToUp);
+}
+
 $(document).ready(function() {
+    // fade effect for navigation
     $("#nav_bar li.nav_button a").not(".selected").hover(function() {
-        $(this).stop().fadeTo(500, 0.7);
+        fadeDown(this);
     }, function() {
-        $(this).stop().fadeTo(500, 1);
+        fadeUp(this);
     });
 
     $("#subnav_top li.subnav_button a").not(".selected").hover(function() {
-        $(this).stop().fadeTo(500, 0.7);
+        fadeDown(this);
     }, function() {
-        $(this).stop().fadeTo(500, 1);
+        fadeUp(this);
     });
 
+    // fade effect for home page
+    $("#tags_home").hover(function() {
+        fadeDown(this);
+    }, function() {
+        fadeUp(this);
+    });
+
+    $("#post_notes_home").hover(function() {
+        fadeDown(this);
+    }, function() {
+        fadeUp(this);
+    });
+
+    $("#post_its_home").hover(function() {
+        fadeDown(this);
+    }, function() {
+        fadeUp(this);
+    });
+
+    $("#projects_home").hover(function() {
+        fadeDown(this);
+    }, function() {
+        fadeUp(this);
+    });
+
+    // underline bottom menu items
     $("#subnav_bottom li.nav_button a").not(".selected").hover(function() {
         $(this).css("text-decoration", "underline");
     }, function() {
         $(this).css("text-decoration", "none");
     });
 
+    // add glass effect
     $("#nav_bar").prepend("<div class='nav_bar_glass_effect'></div>");
     $("#nav_bar li.nav_button a").prepend("<div class='nav_button_glass_effect'></div>");
     $("#subnav_top li.subnav_button a").prepend("<div class='subnav_button_glass_effect'></div>");
     $("#footer").prepend("<div class='footer_glass_effect'></div>");
 
-    $("#post_it_submit").click(function() {
-        $("#post_saved").show("slow").delay(2000).hide("slow");
-    });
-
+    // greeting message on instead top submenu
     var greetColor = $("#nav_bar ul li a").css("color");
     $("li.nav_button.selected#home").each(function() {
         $("#subnav_top").html("<div style='text-align: center; line-height: 1.5em; font-size: 1em; " +
@@ -40,6 +77,7 @@ $(document).ready(function() {
                 "font-weight: bold; color:" + greetColor + ";'>Привет!</div>");
     });
 
+    // drop up menu in bottom submenu
     $("#subnav_bottom ul.navigation li.nav_button").hover(function() {
         $(this).find("ul").hide().show("slow");
     }, function() {
